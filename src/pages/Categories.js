cbc2e16fa9ef1907a31b55565dc42718dee17bb5
masangana <alexmasangana@gmail.com>
2022-07-12 22:32:15 +0300

    Add spin in categories page

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,30 @@
+import React, { useState } from 'react';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+
 function Categories() {
+  const [status, setStatus] = useState('');
+  const handleClick = () => {
+    setStatus('Page Under Construction');
+  };
+  const antIcon = (
+    <LoadingOutlined
+      style={{
+        fontSize: 24,
+      }}
+      spin
+    />
+  );
   return (
     <section className="headerWrapper">
       <div className="headerContainer">
-        <span className="Bookstore-CMS">Categories</span>
-        <button>Check status</button>
+        <Spin indicator={antIcon} />
+        <button type="button" className="statusBtn" onClick={handleClick}>
+          Check status
+        </button>
+        <span className="statusMsg">
+          {status}
+        </span>
       </div>
     </section>
   );
