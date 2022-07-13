4a38d6aac2eec0a96ee0c42af6321572c2be0453
masangana <alexmasangana@gmail.com>
2022-07-13 09:34:21 +0300

    fix linters errors

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -11,9 +11,9 @@ function BookList(props) {
     color: '#888',
     textAlign: 'center',
   };
-  
+
   if (!books.length) return <h1 style={headerStyle}>There are no books to view.</h1>;
-  
+
   return (
     <div className="bookListContainer">
       {books.map((book) => <BookCard key={book.item_id} book={book} />)}
