49f7c8c33c017f8e365d3f09c615838654007f34
masangana <alexmasangana@gmail.com>
2022-07-14 17:52:37 +0300

    add book to the api

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -10,20 +10,17 @@ function BookList(props) {
 
   useEffect(() => {
     dispatch(fetchBookApi());
-  }, []);
+  }, [books.length]);
 
-  const headerStyle = {
-    color: '#888',
-    textAlign: 'center',
-  };
+  if (books.length) {
+    return (
+      <div className="bookListContainer">
+        {books[0].map((book) => <BookCard key={book.item_id} book={book} />)}
+      </div>
+    );
+  }
 
-  if (!books.length) return <h1 style={headerStyle}>There are no books to view.</h1>;
-
-  return (
-    <div className="bookListContainer">
-      { books.length > 0 && books[0].map((book) => <BookCard key={uuidv4()} book={book} />) }
-    </div>
-  );
+  return (<h1 className="headerStyle">There are no books to view.</h1>);
 }
 
 export default BookList;
